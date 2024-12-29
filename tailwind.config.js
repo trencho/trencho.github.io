6fe71270d3ff0286bbf1dec54420e22900cc3803
Aleksandar Trenchevski <atrenchevski@gmail.com>
2024-12-29 15:22:56 +0100

    Added favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   darkMode: "class",
   content: [
-    "./index.html", // Ensure this matches the location of your HTML file
+    "./public/index.html", // Ensure this matches the location of your HTML file
     "./src/**/*.{js,ts,jsx,tsx}", // This covers all JS, TS, JSX, and TSX files in your src directory
   ],
   theme: {
