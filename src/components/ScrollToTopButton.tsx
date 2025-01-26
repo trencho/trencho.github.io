ac3aea2a05b865b626aaaa327a92914c69dd5f28
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-01-26 11:53:38 +0100

    Updated dependencies

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -32,7 +32,7 @@ const ScrollToTopButton = () => {
       {showScrollButton && (
         <motion.button
           onClick={scrollToTop}
-          className="fixed bottom-8 right-6 sm:bottom-10 sm:right-8 md:bottom-12 md:right-10 lg:bottom-16 lg:right-10 text-white rounded-full h-10 w-10 sm:h-12 sm:w-12 flex items-center justify-center shadow-lg"
+          className="fixed bottom-8 right-6 sm:bottom-10 sm:right-8 md:bottom-12 md:right-10 lg:bottom-16 lg:right-10 text-white rounded-full h-10 w-10 sm:h-12 sm:w-12 flex items-center justify-center shadow-lg cursor-pointer"
           title="Back to Top"
           initial={{ y: 100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -44,8 +44,8 @@ const ScrollToTopButton = () => {
           }}
           style={{
             background: darkMode
-              ? "#4fd1c5" // Dark mode color
-              : "#68d391", // Light mode gradient
+              ? "#4fd1c5"
+              : "#68d391",
           }}
           whileHover={{ scale: 1.2 }}
         >
