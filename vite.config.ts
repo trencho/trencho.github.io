80787eca6491a1a3ef609eaecbf3f413db4f10ed
Aleksandar Trenchevski <atrenchevski@gmail.com>
2026-03-31 00:49:27 +0200

    Updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react({ jsxRuntime: 'automatic', babel: { plugins: [] } }),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  plugins: [react({ jsxRuntime: 'automatic' }), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: { port: 3000, open: true },
   build: {
     target: 'esnext',
